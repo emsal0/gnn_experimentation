bdecdfaa445e3fdccf4bce22395f087027fc9eaf
Emmanuel Sales <em@emsal.me>
2021-12-19 12:31:31 -0800

    slicing working on Dataset object

--- process_script.py
+++ process_script.py
@@ -20,17 +20,23 @@ class QM9Dataset(Dataset):
         return len(os.listdir(self.datadir))
 
     def __getitem__(self, idx):
+        if type(idx) is slice:
+            res = [self.__getitem__(i) 
+                    for i in range(*list(filter(None, [idx.start, idx.stop, idx.step])))]
+            return res
         fname = os.path.join(self.datadir, self.prefix + '_' + f"{idx:06}" + '.xyz')
         dat = process(fname)
         nx_graph = rdkit_process(dat)
         return from_networkx(nx_graph)
     
 qm9 = QM9Dataset('xyzfiles', 'dsgdb9nsd')
-mol = qm9[1000]
-print(mol)
 nn = Seq(
         Lin(4, 32), ReLU(), Lin(32, 1))
 
 conv = GINEConv(nn, train_eps=True, edge_dim=1)
-processed = conv(mol.coord.float(), mol.edge_index, mol.bond_type.float().view(-1,1))
-print(processed)
+for i in range(1000+1,1000+11):
+    mol = qm9[i]
+    print(mol.bond_type)
+    processed = conv(mol.coord.float(), mol.edge_index, mol.bond_type.float().view(-1,1))
+
+    print(processed)
